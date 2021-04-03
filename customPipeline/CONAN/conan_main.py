3ef65c259ee6bdacbd5809051e77c050ba779084
gromovnik1337 <vice.roncevic@gmail.com>
2021-04-03 20:15:25 +0200

    Created human segmentation standalone script, started to work on 2ANN inference architecture

---
+++ customPipeline/CONAN/conan_main.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python3
+
+from conan_utils import *
+import depthai as dai
+import numpy as np
+from imutils.video import FPS
+import threading
+import time
+import cv2
+
+# Definition of a pipeline
+# ----------------------------------------------------------------------------
+def create_pipeline():
+    pipeline = dai.Pipeline()
+    # Copy pasted from human segmentation file, reason unknown :)
+    pipeline.setOpenVINOVersion(version = dai.OpenVINO.Version.VERSION_2021_2)
+
+    # Define camera object compatible with deeplabv3 input
+    rgb = pipeline.createColorCamera()
+    rgb.setPreviewSize(nn_shape_1, nn_shape_1)
+    rgb.setInterleaved(False)
+    
+    # Define a ANN node with deeplabv3_person_256 input
+    nn_1 = pipeline.createNeuralNetwork()
+    nn_1.setBlobPath(nn_path_1)
+
+    # Parameters relevant for deepblabv3
+    nn_1.setNumPoolFrames(4)
+    nn_1.input.setBlocking(False) # Queue behavior when full
+    nn_1.setNumInferenceThreads(2) # Threads used by the node to run the inference
+
+    # Link the preview output of the camera with the input to ANN
+    rgb.preview.link(nn_1.input)
+
+    rgb.setFps(40)
+
+    # Define outputs
+    xout_rgb = pipeline.createXLinkOut()
+    xout_rgb.setStreamName("rgb_stream_1")
+
+    xout_nn_1 = pipeline.createXLinkOut()
+    xout_nn_1.setStreamName("nn_1")
+
+    # Overwrite default input queue behavior from blocking to overwriting
+    # Why?
+    xout_rgb.input.setBlocking(False)
+    xout_nn_1.input.setBlocking(False)
+
+    # This links the passthrough message with the XLink output from the camera
+    # Why? Maybe because of the nature of the output from deeplab? Heatmap?
+    # N.B. Passthrough message is suitable for when the queue is set to overwriting
+    nn_1.passthrough.link(xout_rgb.input)   
+
+    # Send the inference data to the host
+    nn_1.out.link(xout_nn_1.input)
+
+# ---------------------------------------------------------------------------- 
+
+    # Change the parameters to fit the OpenPose
+    rgb.setPreviewSize(nn_shape_2_x, nn_shape_2_y)
+    rgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+    rgb.setInterleaved(False)
+    rgb.setBoardSocket(dai.CameraBoardSocket.RGB)
+    
+    # Define a ANN node with deeplabv3_person_256 input
+    nn_2 = pipeline.createNeuralNetwork()
+    nn_2.setBlobPath(nn_path_2)
+
+    # Parameters relevant for OpenPose
+    nn_2.setNumInferenceThreads(2)
+    nn_2.input.setQueueSize(1)
+    nn_2.input.setBlocking(False) # Queue behavior when full
+    nn_1.setNumInferenceThreads(2) # Threads used by the node to run the inference
+
+    # Link the preview output of the camera with the input to ANN
+    rgb.preview.link(nn_2.input)
+
+    # Define outputs
+    xout_rgb = pipeline.createXLinkOut()
+    xout_rgb.setStreamName("rgb_stream_2")
+
+    xout_nn_2 = pipeline.createXLinkOut()
+    xout_nn_2.setStreamName("nn_2")
+
+    nn_2.out.link(xout_rgb.input)   
+
+    # Send the inference data to the host
+    nn_2.out.link(xout_nn_2.input)
+
+    return pipeline
+
+# ----------------------------------------------------------------------------
+
+with dai.Device(create_pipeline()) as device:
+    print("Starting pipeline...")
+    device.startPipeline()
+
+    # Host side queues, parameters must correspond those from the pipeline inputs 
+    q_rgb = device.getOutputQueue(name = "rgb_stream_1", maxSize = 4, blocking = False)
+    q_nn = device.getOutputQueue(name = "nn_1", maxSize = 4, blocking = False)
+
+    # Main program loop
+    # ----------------------------------------------------------------------------
+
+    # Auxiliary variables relevant for output
+    start_time = time.time()
+    counter = 0
+    fps = 0
+    layer_info_printed = False
+
+    while True:
+        # Fetch latest results
+        # tryGet() will return available data or return None otherwise
+        in_rgb = q_rgb.get()
+        in_nn = q_nn.get()
+
+        # RGB camera input (1D array) conversion into Height-Width-Channels (HWC) form
+        if in_rgb is not None:
+            shape = (3, in_rgb.getHeight(), in_rgb.getWidth())
+            frame = in_rgb.getData().reshape(shape).transpose(1, 2, 0).astype(np.uint8)
+            frame = np.ascontiguousarray(frame)
+
+        # ANN results (1D array, fixed size, no matter how much results ANN has produced, results end with -1, the rest is filled with 0s) transformations 
+        if in_nn is not None:
+            # print("NN received")
+            # https://docs.luxonis.com/projects/api/en/latest/references/python/#depthai.NNData.getAllLayers
+            # https://docs.luxonis.com/projects/api/en/latest/references/python/#depthai.TensorInfo
+            layers = in_nn.getAllLayers()
+
+            # Print the info acquired from the deepplabv3
+            if not layer_info_printed:
+                for layer_nr, layer in enumerate(layers):
+                    print(f"Layer {layer_nr}")
+                    print(f"Name: {layer.name}")
+                    print(f"Order: {layer.order}")
+                    print(f"dataType: {layer.dataType}")
+                    dims = layer.dims[::-1] # reverse dimensions
+                    print(f"dims: {dims}")
+                layer_info_printed = True
+
+            # Relevant information is in layer1
+            # getLayerInt32 takes layer name as an argument it is different from getFirstLayerInt32
+            layer_1 = in_nn.getLayerInt32(layers[0].name)
+
+            # Create numpy output
+            ims = layer.dims[::-1]
+            layer_1 = np.asarray(layer_1, dtype=np.int32).reshape(dims)
+
+            output_colors = decode_deeplabv3p(layer_1)
+
+            if frame is not None:
+                frame = show_deeplabv3p(output_colors, frame)
+                cv2.putText(frame, "NN fps: {:.2f}".format(fps), (2, frame.shape[0] - 4), cv2.FONT_HERSHEY_TRIPLEX, 0.4, (255, 0, 0))
+                cv2.imshow("nn_input", frame)
+        
+        counter+=1
+        if (time.time() - start_time) > 1 :
+            fps = counter / (time.time() - start_time)
+
+            counter = 0
+            start_time = time.time()
+
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+   
---
+++ customPipeline/CONAN/conan_utils.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import cv2
+
+# Path of the deeplabv3 model
+nn_path_1 = "./deeplab_v3_plus_mvn2_decoder_256_openvino_2021.2_6shave.blob"
+# nn_path = "./deeplab_v3_plus_mvn2_decoder_513_openvino_2021.2_6shave.blob"
+# nn_path = "./deeplab_v3_plus_mvn3_decoder_256_openvino_2021.2_6shave.blob"
+
+nn_shape_1 = 256
+if '513' in nn_path_1:
+    nn_shape_1 = 513
+
+# Path of the OpenPose model
+
+nn_path_2 = "./human-pose-estimation-0001_openvino_2021.2_6shave.blob"
+
+nn_shape_2_x = 456
+nn_shape_2_y = 256
+
+def decode_deeplabv3p(output_tensor):
+    class_colors = [[0,0,0],  [0,255,0]]
+    class_colors = np.asarray(class_colors, dtype=np.uint8)
+    
+    # 1-D output of the layer_1 has to be re-shaped to have the same shape as the input to ANN
+    # In the simple case 256 px x 256 px
+    output = output_tensor.reshape(nn_shape, nn_shape)
+    # Take only the indices of the full color array that contain the person
+    output_colors = np.take(class_colors, output, axis=0)
+    return output_colors
+
+def show_deeplabv3p(output_colors, frame):
+    # Adds two images together but each is assigned different weight
+    # https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_core/py_image_arithmetics/py_image_arithmetics.html
+    # This will superimpose color "person blob" over the frame
+    return cv2.addWeighted(frame,1, output_colors,0.2,0)
